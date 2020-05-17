3b1a0cd348560f225b533c482bc9425ddc2e8201
banana <jgjin@umich.edu>
2020-05-17 06:16:49 -0400

    album json type

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
 
@@ -37,4 +37,68 @@ pub struct Copyright {
     pub text: String,
     #[serde(rename = "type")]
     pub copyright_type: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct AlbumJson {
+    pub artists: String,
+    pub image_url: String,
+    pub name: String,
+    pub external_url: String,
+    pub copyright: String,
+}
+
+impl From<SavedAlbum> for AlbumJson {
+    fn from(saved_album: SavedAlbum) -> Self {
+        Self {
+            artists: saved_album
+                .album
+                .artists
+                .iter()
+                .map(|artist| artist.name.clone())
+                .collect::<Vec<String>>()
+                .join(", "),
+            image_url: saved_album
+                .album
+                .images
+                .iter()
+                .next()
+                .map(|image| image.url.clone())
+                .unwrap_or("".to_string()),
+            name: saved_album.album.name,
+            external_url: saved_album
+                .album
+                .external_urls
+                .get("spotify")
+                .map(|external_url| external_url.clone())
+                .unwrap_or("".to_string()),
+            copyright: saved_album
+                .album
+                .copyrights
+                .iter()
+                .filter(|copyright| copyright.copyright_type == "C")
+                .next()
+                .or(saved_album.album.copyrights.first())
+                .and_then(|copyright| match &copyright.copyright_type[..] {
+                    "C" => {
+                        let mut cleaned = copyright.text.replace("(C)", "©");
+                        if !cleaned.starts_with("©") && !cleaned.starts_with("℗") {
+                            cleaned = format!("© {}", cleaned);
+                        }
+
+                        Some(cleaned)
+                    }
+                    "P" => {
+                        let mut cleaned = copyright.text.replace("(P)", "℗");
+                        if !cleaned.starts_with("℗") && !cleaned.starts_with("©") {
+                            cleaned = format!("℗ {}", cleaned);
+                        }
+
+                        Some(cleaned)
+                    }
+                    _ => None,
+                })
+                .unwrap_or("".to_string()),
+        }
+    }
 }
